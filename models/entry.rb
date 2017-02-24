313d6e68e9b7ac93cb5889df40bdd3fdb9f9f4c3
Sam Dieck <samuel.dieck@gmail.com>
2017-02-23 20:49:46 -0800

    Checkpoint 2 stuff

--- models/entry.rb
+++ models/entry.rb
@@ -1,12 +1,8 @@
-class Entry
+require 'bloc_record/base'
+
+class Entry < BlocRecord::Base
   # These must be accessors since we mutate them
   attr_accessor :name, :phone_number, :email
-
-  def initialize(name, phone_number, email)
-    @name = name
-    @phone_number = phone_number
-    @email = email
-  end
 
   def to_s
     "Name: #{name}\nPhone Number: #{phone_number}\nEmail: #{email}"
